8ff3397a21e95ba6d6065b911d0c395d0b25d071
Bruno A. Tretto <bruno@Bruno.localdomain>
2026-06-02 11:57:05 -0300

    feat: adiciona interface das estrategias de jogo, basendo no desing Strategy

--- app/src/main/java/hexafoot/model/strategy/TaticaEquilibrada.java
+++ app/src/main/java/hexafoot/model/strategy/TaticaEquilibrada.java
@@ -1,5 +1,24 @@
 package hexafoot.model.strategy;
 
 public class TaticaEquilibrada implements EstrategiaSimulacao {
+    //tática padrão, sem modificadores
+    // Formações equilibradas
+    // 4-4-2
+    // 4-2-3-1
+    // 3-5-2
+    
+    @Override
+    public double getModificadorAtaque() {
+        return 1.0; 
+    }
 
+    @Override
+    public double getModificadorDefesa() {
+        return 1.0; 
+    }
+
+    @Override
+    public double getMultiplicadorDesgaste() {
+        return 1.0;
+    }
 }
--- app/src/main/java/hexafoot/model/strategy/TaticaOfensiva.java
+++ app/src/main/java/hexafoot/model/strategy/TaticaOfensiva.java
@@ -1,5 +1,23 @@
 package hexafoot.model.strategy;
 
 public class TaticaOfensiva implements EstrategiaSimulacao {
+    // Formações ofensivas
+    // 4-3-3
+    // 3-4-3
+    // 4-2-4
+    
+    @Override
+    public double getModificadorAtaque() {
+        return 1.1; // Aumenta o ataque em 10%
+    }
 
+    @Override
+    public double getModificadorDefesa() {
+        return 0.9; // Diminui a defesa em 10%
+    }
+
+    @Override
+    public double getMultiplicadorDesgaste() {
+        return 1.1; // Aumenta o desgaste em 10%
+    }
 }
