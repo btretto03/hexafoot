abb47df287d039e93894fde24a30e760647ec334
Bruno A. Tretto <bruno@Bruno.localdomain>
2026-06-02 23:22:45 -0300

    feat: adicionando processador físico(observer)

--- app/src/main/java/hexafoot/service/simulacao/ProcessadorFisico.java
+++ app/src/main/java/hexafoot/service/simulacao/ProcessadorFisico.java
@@ -9,15 +9,23 @@ import hexafoot.model.Partida;
  */
 
 public class ProcessadorFisico implements ObserverMinuto {
+
     @Override
     public void atualizarMinuto(int minutoAtual, Partida partida) {
-        
+
+        float multiplicadorMandante = (float) partida.getMandante().getTaticaAtual().getMultiplicadorDesgaste();
+        float multiplicadorVisitante = (float) partida.getVisitante().getTaticaAtual().getMultiplicadorDesgaste();
+
         for (Jogador jogador : partida.getMandante().getTitulares()) {
-            jogador.consumirEnergia(1, 1.0f);
+            if ("Ativo".equals(jogador.getStatus())) {
+                jogador.consumirEnergia(1, multiplicadorMandante);
+            }
         }
-        
+
         for (Jogador jogador : partida.getVisitante().getTitulares()) {
-            jogador.consumirEnergia(1, 1.0f);
+            if ("Ativo".equals(jogador.getStatus())) {
+                jogador.consumirEnergia(1, multiplicadorVisitante);
+            }
         }
     }
 }
